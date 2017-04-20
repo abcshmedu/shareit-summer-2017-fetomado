66e7e01bf074f66cd9fec3df67e170154adf6c65
DomixD <dominik-grenz@t-online.de>
2017-04-20 19:27:56 +0200

    Add Copy class with javadoc

---
+++ src/main/java/edu/hm/shareit/models/Copy.java
@@ -0,0 +1,36 @@
+package edu.hm.shareit.models;
+
+/**
+ * This class represents a copy of a medium.
+ */
+public class Copy {
+
+	private Medium medium;
+	private String owner;
+
+	/**
+	 * Constructs a new copy of a medium.
+	 * @param owner the owner of the copy
+	 * @param medium the medium of the copy
+	 */
+	public Copy(String owner, Medium medium) {
+		this.owner = owner;
+		this.medium = medium;
+	}
+	
+	/**
+	 * Returns the medium of the copy.
+	 * @return the medium
+	 */
+	public Medium getMedium(){
+		return medium;
+	}
+	
+	/**
+	 * Returns the owner of the copy.
+	 * @return the owner
+	 */
+	public String getOwner(){
+		return owner;
+	}
+}

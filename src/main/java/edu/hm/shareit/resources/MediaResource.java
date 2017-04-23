4c4bece4ed940f1790ed4860a452257ba61adafb
Tobias Kroiss <tobi.kroiss@googlemail.com>
2017-04-23 15:13:36 +0200

    Add "toJson()"

--- src/main/java/edu/hm/shareit/resources/MediaResource.java
+++ src/main/java/edu/hm/shareit/resources/MediaResource.java
@@ -1,24 +1,20 @@
 package edu.hm.shareit.resources;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.hm.shareit.models.Book;
-import edu.hm.shareit.models.Medium;
 import edu.hm.shareit.services.MediaService;
 import edu.hm.shareit.services.MediaServiceImpl;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 /**
  * This class is the API-layer for all requests to the resource media.
@@ -57,40 +53,26 @@ public class MediaResource {
     @Path("/books/{isbn}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getBook(@PathParam("isbn") String isbn){
-        ObjectMapper mapper = new ObjectMapper();
-        String json = "";
-        try {
-            json = mapper.writeValueAsString(service.getBook(isbn));
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
         return Response.status(Response.Status.OK)
-                .entity(json)
+                .entity(toJson(service.getBook(isbn)))
                 .build();
     }
     
     /**
-     * This function handles GET request to /media/books/ .
-     * @return response with a list of all books
+     * This function handles GET requests to /media/books/.
+     * @return response list of all books
      */
     @GET
     @Path("/books")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getBooks() {
-        ObjectMapper mapper = new ObjectMapper();
-        String json = "";
-        try {
-            json = mapper.writeValueAsString(service.getBooks());
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
         return Response.status(Response.Status.OK)
-                .entity(json)
+                .entity(toJson(service.getBooks()))
                 .build();
     }
     
     /**
-     *  This function handles GET request to  /media/discs/{barcode} .
+     *  This function handles GET requests to /media/discs/{barcode}.
      * @param barcode
      * @return response a disc with the correct barcode 
      */
@@ -98,36 +80,37 @@ public class MediaResource {
     @Path("/discs/{barcode}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getDisc(@PathParam("barcode") String barcode){
-    	 ObjectMapper mapper = new ObjectMapper();
-         String json = "";
-         try {
-             json = mapper.writeValueAsString(service.getDisc(barcode));
-         } catch (JsonProcessingException e) {
-             e.printStackTrace();
-         }
-    	return Response.status(Response.Status.OK)
-    					.entity(json)
-    					.build();
+        return Response.status(Response.Status.OK)
+                        .entity(toJson(service.getDisc(barcode)))
+                        .build();
     }
     
     /**
-     * This function handles GET request to /media/discs/ .
+     * This function handles GET requests to /media/discs/.
      * @return response with a list of all discs
      */
     @GET
     @Path("/discs")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getDiscs() {
+        return Response.status(Response.Status.OK)
+                .entity(toJson(service.getDiscs()))
+                .build();
+    }
+    
+    /**
+     * This method converts an Object to a string-representation in JSON.
+     * @param obj Object to convert
+     * @return String representation of the JSON-Object
+     */
+    private String toJson(Object obj) {
         ObjectMapper mapper = new ObjectMapper();
-        String json = "";
         try {
-            json = mapper.writeValueAsString(service.getDiscs());
+            return mapper.writeValueAsString(obj);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-        return Response.status(Response.Status.OK)
-                .entity(json)
-                .build();
+        return "";
     }
     
 }

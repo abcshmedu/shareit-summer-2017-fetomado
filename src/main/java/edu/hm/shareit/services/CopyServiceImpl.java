e7201eac673c12eb9ff6778b591dfde215578588
Felix Maurer <felix.m@online.de>
2017-04-26 13:57:44 +0200

    Add test classes for copy

--- src/main/java/edu/hm/shareit/services/CopyServiceImpl.java
+++ src/main/java/edu/hm/shareit/services/CopyServiceImpl.java
@@ -1,4 +1,4 @@
 package edu.hm.shareit.services;
 
-public class CopyServiceImpl {
+public class CopyServiceImpl implements CopyService {
 }
---
+++ src/test/java/edu/hm/shareit/services/CopyServiceImplTest.java
@@ -0,0 +1,12 @@
+package edu.hm.shareit.services;
+
+import org.junit.Test;
+
+public class CopyServiceImplTest {
+
+    @Test
+    public void test() {
+        assert(true);
+    }
+
+}

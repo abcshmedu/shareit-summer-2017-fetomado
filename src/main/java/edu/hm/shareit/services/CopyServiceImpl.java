6101dc8069b27683676cdf64d975b641af13f49d
DomixD <dominik-grenz@t-online.de>
2017-04-28 14:33:24 +0200

    Update addCopy() with ServiceResults

--- src/main/java/edu/hm/shareit/services/CopyServiceImpl.java
+++ src/main/java/edu/hm/shareit/services/CopyServiceImpl.java
@@ -3,6 +3,7 @@ package edu.hm.shareit.services;
 import edu.hm.shareit.models.Book;
 import edu.hm.shareit.models.Copy;
 import edu.hm.shareit.models.Disc;
+import edu.hm.shareit.models.Medium;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -26,20 +27,21 @@ public class CopyServiceImpl implements CopyService {
     public ServiceResult addCopy(String owner, String medium) {
         MediaServiceImpl tmpMsr = new MediaServiceImpl();
         Copy copy;
+        Medium mediumToCopy = null;
         String regexISBN = "^(?:ISBN(?:-13)?:? )?(?=[0-9]{13}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)97[89][- ]?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9]$";
         String regexBarcode = "^[1-9][0-9]{8,14}$";
         Pattern patternISBN = Pattern.compile(regexISBN);
         Pattern patternBarcode = Pattern.compile(regexBarcode);
         if (patternISBN.matcher(medium).matches()) {
-            Book book = tmpMsr.getBook(medium);
-            copy = new Copy(owner, book);
-            copys.put(copy.getId(), copy);
+            mediumToCopy = tmpMsr.getBook(medium);
         } else if (patternBarcode.matcher(medium).matches()) {
-            Disc disc = tmpMsr.getDisc(medium);
-            copy = new Copy(owner, disc);
-            copys.put(copy.getId(), copy);
+            mediumToCopy = tmpMsr.getDisc(medium);
         }
-
+        if (mediumToCopy == null) {
+            return ServiceResult.NOT_FOUND;
+        }
+        copy = new Copy(owner, mediumToCopy);
+        copys.put(copy.getId(), copy);
         return ServiceResult.OK;
     }
 

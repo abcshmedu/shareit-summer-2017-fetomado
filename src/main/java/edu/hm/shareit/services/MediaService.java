45f93e5e3f1382cc83a072f2a629b44bb2a17604
DomixD <dominik-grenz@t-online.de>
2017-05-04 13:04:53 +0200

    Add javadoc getBook(), getBooks(), getDisc(), getDiscs()

--- src/main/java/edu/hm/shareit/services/MediaService.java
+++ src/main/java/edu/hm/shareit/services/MediaService.java
@@ -8,14 +8,32 @@ public interface MediaService {
 
     ServiceResult addDisc(Disc disc);
 
+    /**
+     * Returns the book with unique ISBN number.
+     * @param isbn unique identifier of the book
+     * @return the book is it exists, null otherwise
+     */
     Book getBook(String isbn);
 
+    /**
+     * Returns all books that exist in the service.
+     * @return an array with all books
+     */
     Book[] getBooks();
 
     ServiceResult updateBook(String isbn, Book book);
 
+    /**
+     * Returns the disc with unique Barcode.
+     * @param barcode unique identifier of the disc
+     * @return the disc is it exists, null otherwise
+     */
     Disc getDisc(String barcode);
 
+    /**
+     * Returns all discs that exist in the service.
+     * @return an array with all discs
+     */
     Disc[] getDiscs();
 
     ServiceResult updateDisc(String barcode, Disc disc);

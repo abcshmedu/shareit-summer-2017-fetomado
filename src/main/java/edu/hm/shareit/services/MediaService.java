e84e72f6914cfb69f785c4d08d20fd2056bbeefc
Tobias Kroiss <tobi.kroiss@googlemail.com>
2017-05-04 13:09:51 +0200

    Add javadoc updateBook() & updateDisc()

--- src/main/java/edu/hm/shareit/services/MediaService.java
+++ src/main/java/edu/hm/shareit/services/MediaService.java
@@ -12,11 +12,23 @@ public interface MediaService {
 
     Book[] getBooks();
 
+    /**
+     * This function is used to change the author or the title of an existing book.
+     * @param isbn unique identifier of the book to be changed
+     * @param book book that contains the edited values
+     * @return ServiceResult depending on the behaviour of the update-process
+     */
     ServiceResult updateBook(String isbn, Book book);
 
     Disc getDisc(String barcode);
 
     Disc[] getDiscs();
 
+    /**
+     * This function is used to change the director, title or fsk of an existing disc.
+     * @param barcode unique identifier of the disc to be changed
+     * @param disc disc that contains the edited values
+     * @return ServiceResult depending on the behaviour of the update-process
+     */
     ServiceResult updateDisc(String barcode, Disc disc);
 }

4ed32589deecc8e180d35c0749c74c84a2b9f530
Marita Segl <maritasegl@gmx.de>
2017-05-27 13:54:23 +0200

    Fix tests in CopyResourceTest

--- src/test/java/edu/hm/shareit/resources/CopyResourceTest.java
+++ src/test/java/edu/hm/shareit/resources/CopyResourceTest.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import edu.hm.shareit.models.Book;
 import edu.hm.shareit.models.Copy;
+import edu.hm.shareit.models.User;
 import edu.hm.shareit.services.CopyService;
 import edu.hm.shareit.services.ServiceResult;
+import edu.hm.shareit.services.UserService;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
@@ -25,26 +27,31 @@ import static org.mockito.Mockito.when;
 public class CopyResourceTest extends JerseyTest {
 
     @Mock
-    private CopyService serviceMock;
+    private CopyService CopyServiceMock;
     private Book book = new Book("Die Kaenguru-Chroniken", "Marc-Uwe Kling", "978-3-548-37623-3");
     private Copy[] copies = {
             new Copy("Test User", book),
             new Copy("Egon", book),
     };
 
+    @Mock
+    private UserService UserServiceMock;
+
     @Override
     protected Application configure() {
         MockitoAnnotations.initMocks(this);
-        return new ResourceConfig().register(new CopyResource(serviceMock))
+        return new ResourceConfig().register(new CopyResource(CopyServiceMock))
                 .register(CheckTokenRequestFilter.class);
     }
 
     @Test
     public void testCreateCopy() {
-        when(serviceMock.addCopy(any(String.class), any(String.class))).thenReturn(ServiceResult.OK);
+        when(CopyServiceMock.addCopy(any(String.class), any(String.class))).thenReturn(ServiceResult.OK);
+        when(UserServiceMock.checkToken("abc")).thenReturn(new User("TestU", "Test"));
         ObjectNode root = new ObjectMapper().createObjectNode();
         root.put("medium", "978-3-548-37623-3");
         root.put("owner", "Test User");
+        root.put("token", "abc");
         Entity<ObjectNode> json = Entity.entity(root, MediaType.APPLICATION_JSON);
         Response resp = target("copies").request().post(json);
         assertEquals(200, resp.getStatus());
@@ -55,7 +62,7 @@ public class CopyResourceTest extends JerseyTest {
     public void testGetCopies() {
         int id1 = copies[0].getId();
         int id2 = copies[1].getId();
-        when(serviceMock.getCopies()).thenReturn(copies);
+        when(CopyServiceMock.getCopies()).thenReturn(copies);
         Response resp = target("copies").request().get();
         assertEquals(200, resp.getStatus());
         assertEquals("[{\"medium\":{\"title\":\"Die Kaenguru-Chroniken\",\"author\":\"Marc-Uwe Kling\",\"isbn\":\"978-3-548-37623-3\"},\"owner\":\"Test User\",\"id\":" + id1 + "}," +
@@ -66,7 +73,7 @@ public class CopyResourceTest extends JerseyTest {
     @Test
     public void testGetCopy() {
         int id = copies[1].getId();
-        when(serviceMock.getCopy(id)).thenReturn(copies[1]);
+        when(CopyServiceMock.getCopy(id)).thenReturn(copies[1]);
         Response resp = target("copies/" + id).request().get();
         assertEquals(200, resp.getStatus());
         assertEquals("{\"medium\":{\"title\":\"Die Kaenguru-Chroniken\",\"author\":\"Marc-Uwe Kling\",\"isbn\":\"978-3-548-37623-3\"},\"owner\":\"Egon\",\"id\":" + id + "}",
@@ -76,9 +83,11 @@ public class CopyResourceTest extends JerseyTest {
     @Test
     public void testUpdateCopy() {
         int id = copies[1].getId();
-        when(serviceMock.updateCopy(eq(id), any(String.class))).thenReturn(ServiceResult.OK);
+        when(CopyServiceMock.updateCopy(eq(id), any(String.class))).thenReturn(ServiceResult.OK);
+        when(UserServiceMock.checkToken("testToken")).thenReturn(new User("TestUser", "testPw"));
         ObjectNode root = new ObjectMapper().createObjectNode();
         root.put("owner", "Test User");
+        root.put("token", "testToken");
         Entity<ObjectNode> json = Entity.entity(root, MediaType.APPLICATION_JSON);
         Response resp = target("copies/" + id).request().put(json);
         assertEquals(200, resp.getStatus());

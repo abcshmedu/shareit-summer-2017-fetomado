43be2c4df69e1a3da7df70c9dd367a3fa658f275
DomixD <dominik-grenz@t-online.de>
2017-05-28 11:01:27 +0200

    Add javadoc for User.java and Token.java

--- src/main/java/edu/hm/shareit/models/Token.java
+++ src/main/java/edu/hm/shareit/models/Token.java
@@ -2,14 +2,24 @@ package edu.hm.shareit.models;
 
 import java.util.UUID;
 
+/**
+ * This class creates a token with a random UUID.
+ */
 public class Token {
 
     private String token;
 
+    /**
+     * Constructs a new token object with a random UUID.
+     */
     public Token() {
         token = UUID.randomUUID().toString();
     }
 
+    /**
+     * Returns the token.
+     * @return the token
+     */
     public String getToken() {
         return token;
     }
--- src/main/java/edu/hm/shareit/models/User.java
+++ src/main/java/edu/hm/shareit/models/User.java
@@ -1,21 +1,40 @@
 package edu.hm.shareit.models;
 
+/**
+ * This is the model class representing an user.
+ */
 public class User {
 
     private String username, password;
 
+    /**
+     * Private constructor needed for Jackson compatibility
+     */
+    private User() { };
+
+    /**
+     * Constructs a new user object.
+     * @param username the username of the user
+     * @param pwd the password of the user
+     */
     public User(String username, String pwd) {
         this.username = username;
         this.password = pwd;
     }
 
+    /**
+     * Returns the username of the user.
+     * @return username of the user
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Returns the password of the user.
+     * @return password of the user
+     */
     public String getPassword() {
         return password;
     }
-
-    private User() {};
 }

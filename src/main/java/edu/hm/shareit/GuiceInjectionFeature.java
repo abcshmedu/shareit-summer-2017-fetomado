b3308d9c5e8e5125aaac356b093413aa8e324457
Felix Maurer <felix.m@online.de>
2017-06-13 00:46:05 +0200

    Move injections into one class
    All injection related stuff can now be found in one single file. This
    makes it more straightforward and allows it to be easily applied to the
    test infrastructure.

--- src/main/java/edu/hm/shareit/GuiceInjectionFeature.java
+++ src/main/java/edu/hm/shareit/GuiceInjectionFeature.java
@@ -1,5 +1,9 @@
 package edu.hm.shareit;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import edu.hm.shareit.services.*;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.jersey.ServiceLocatorProvider;
 import org.jvnet.hk2.guice.bridge.api.GuiceBridge;
@@ -12,12 +16,29 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class GuiceInjectionFeature implements Feature {
 
+    private static final Injector injector = Guice.createInjector(new AbstractModule() {
+        @Override
+        protected void configure() {
+            bind(MediaService.class).to(MediaServiceImpl.class);
+            bind(CopyService.class).to(CopyServiceImpl.class);
+            bind(UserService.class).to(UserServiceImpl.class);
+        }
+    });
+
     public boolean configure(FeatureContext context) {
         ServiceLocator serviceLocator = ServiceLocatorProvider.getServiceLocator(context);
         GuiceBridge.getGuiceBridge().initializeGuiceBridge(serviceLocator);
         GuiceIntoHK2Bridge guiceBridge = serviceLocator.getService(GuiceIntoHK2Bridge.class);
-        guiceBridge.bridgeGuiceInjector(ShareitInjector.getInjectorInstance());
+        guiceBridge.bridgeGuiceInjector(getInjectorInstance());
         return true;
+    }
+
+    /**
+     * This method is only required for the HK2-Guice-Bridge in the Application class.
+     * @return Injector instance.
+     */
+    static Injector getInjectorInstance() {
+        return injector;
     }
 
 }

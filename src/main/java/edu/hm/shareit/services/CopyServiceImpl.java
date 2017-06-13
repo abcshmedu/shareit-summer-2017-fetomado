64d6be12cc5bd440247496c38ada717baf825db6
Tobias Kroiss <tobi.kroiss@googlemail.com>
2017-06-13 16:39:05 +0200

    Edit CopyServiceImpl to use PersistenceImpl

--- src/main/java/edu/hm/shareit/services/CopyServiceImpl.java
+++ src/main/java/edu/hm/shareit/services/CopyServiceImpl.java
@@ -1,10 +1,14 @@
 package edu.hm.shareit.services;
 
+import edu.hm.shareit.models.Book;
 import edu.hm.shareit.models.Copy;
+import edu.hm.shareit.models.Disc;
 import edu.hm.shareit.models.Medium;
+import edu.hm.shareit.persistence.Persistence;
 
+import javax.inject.Inject;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 import java.util.regex.Pattern;
 
 /**
@@ -13,68 +17,58 @@ import java.util.regex.Pattern;
  */
 public class CopyServiceImpl implements CopyService {
 
-    private static Map<Integer, Copy> copies;
-
-    /**
-     * Constructs a new instance.
-     */
-    public CopyServiceImpl() {
-        if (copies == null) {
-            copies = new HashMap<>();
-        }
-    }
+    @Inject
+    private Persistence persist;
 
     @Override
     public ServiceResult addCopy(String owner, String medium) {
-        MediaServiceImpl tmpMsr = new MediaServiceImpl();
-        Copy copy;
         Medium mediumToCopy = null;
         String regexISBN = "^(?:ISBN(?:-13)?:? )?(?=[0-9]{13}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)97[89][- ]?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9]$";
         String regexBarcode = "^[1-9][0-9]{8,14}$";
         Pattern patternISBN = Pattern.compile(regexISBN);
         Pattern patternBarcode = Pattern.compile(regexBarcode);
         if (patternISBN.matcher(medium).matches()) {
-            mediumToCopy = tmpMsr.getBook(medium);
+            mediumToCopy = persist.get(Book.class, medium);
         } else if (patternBarcode.matcher(medium).matches()) {
-            mediumToCopy = tmpMsr.getDisc(medium);
+            mediumToCopy = persist.get(Disc.class, medium);
         }
         if (mediumToCopy == null) {
             return ServiceResult.NOT_FOUND;
         }
-        copy = new Copy(owner, mediumToCopy);
-        copies.put(copy.getId(), copy);
+        Copy copy = new Copy(owner, mediumToCopy);
+        persist.add(copy);
         return ServiceResult.OK;
     }
 
     @Override
     public Copy[] getCopies() {
-        return copies.values().toArray(new Copy[copies.size()]);
+        List<Copy> copies = persist.getAll(Copy.class);
+        return copies.toArray(new Copy[copies.size()]);
     }
 
     @Override
     public Copy getCopy(int id) {
-        return copies.get(id);
+        if (persist.exist(Copy.class, id)) {
+            return persist.get(Copy.class, id);
+        } else {
+            return null;
+        }
     }
 
     @Override
     public ServiceResult updateCopy(int id, String owner) {
-        if (!copies.containsKey(id)) {
+        if (!persist.exist(Copy.class, id)) {
             return ServiceResult.NOT_FOUND;
         }
         if ((owner == null || owner.equals(""))) {
             return ServiceResult.BAD_REQUEST;
         }
-        Copy copyToEdit = copies.get(id);
-        if (owner != null && !owner.equals("")) {
+        Copy copyToEdit = persist.get(Copy.class, id);
+        if (!owner.equals("")) {
             copyToEdit.setOwner(owner);
         }
+        persist.update(copyToEdit);
         return ServiceResult.OK;
     }
 
-    /**
-     * Removes the data stored in this service. This method should mainly be used in tests.
-     */
-    void flushDataForTesting() {
-        copies = new HashMap<>();
-    }
 }

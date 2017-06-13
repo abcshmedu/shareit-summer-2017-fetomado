9268671654db9669b9eeec055267150fe2f815bb
Felix Maurer <felix.m@online.de>
2017-06-13 13:35:47 +0200

    Fix errors from merge
    equals() and hashCode() were doubled in Book

--- src/main/java/edu/hm/shareit/models/Book.java
+++ src/main/java/edu/hm/shareit/models/Book.java
@@ -15,24 +15,6 @@ public class Book extends Medium {
     @Id private String isbn;
     private String author;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Book book = (Book) o;
-
-        if (isbn != null ? !isbn.equals(book.isbn) : book.isbn != null) return false;
-        return author != null ? author.equals(book.author) : book.author == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = isbn != null ? isbn.hashCode() : 0;
-        result = 31 * result + (author != null ? author.hashCode() : 0);
-        return result;
-    }
-
     /**
      * Constructs a new book object.
      * @param title  the title of the book
